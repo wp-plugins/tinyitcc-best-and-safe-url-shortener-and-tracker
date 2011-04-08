6b4cd877f1ed40f44b97b1b8a2b097d507694ade
tinyit <tinyit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-08 03:53:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tinyitcc-best-and-safe-url-shortener-and-tracker/trunk@370458 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- template/public.tpl.php
+++ template/public.tpl.php
@@ -1,11 +1,9 @@
-
 <div style="margin-top:2em;">
   <?php if ($opt['Display'] == 'Y'): ?>
-    <?php echo __('<br><b>Short URL:</b>') ?>
+    <?php echo __('<br><b>Short URL: </b>') ?>
     <a href="<?php echo $shortUrl ?>"><?php echo get_post_meta($post->ID, 'TinyitShortURL', true); ?></a>
   <?php endif ?>
   <?php if ($opt['TwitterLink'] == 'Y'): ?>    
-  <?php echo __('&nbsp; &nbsp; Tweet:') ?>
+  <?php echo __('&nbsp;<b>Tweet: </b>') ?>
 <a href="http://twitter.com/?status=<?php echo $shortUrlEncoded ?>"><img src="http://twitter.com/favicon.ico" title="" alt="" /></a>  <?php endif ?>
 </div>
-
